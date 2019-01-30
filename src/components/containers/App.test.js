dcce3b99abb81bcddc4dff54c609e154835f48cc
DR <dimaka77@gmail.com>
2019-01-29 19:29:32 -0800

    Added: App render test cases

--- src/components/containers/App.test.js
+++ src/components/containers/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+import reducer from '../../reducers/RootReducer';
 import App from './App';
+import renderer from 'react-test-renderer';
+
+const middleware = [thunk, promise];
+const store = createStore(reducer, compose(applyMiddleware(...middleware)));
+const APP = <Provider store={store}><App /></Provider>;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(APP, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(APP)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
